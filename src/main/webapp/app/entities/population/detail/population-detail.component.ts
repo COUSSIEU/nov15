380f3c9e4ed167a0d7766751605d02bfe6b69316
olivier coussieu <olivier.coussieu@orange.fr>
2024-11-18 09:57:33 +0100

    formGroup

--- src/main/webapp/app/entities/population/detail/population-detail.component.ts
+++ src/main/webapp/app/entities/population/detail/population-detail.component.ts
@@ -4,16 +4,20 @@ import { RouterModule } from '@angular/router';
 import SharedModule from 'app/shared/shared.module';
 import { DurationPipe, FormatMediumDatePipe, FormatMediumDatetimePipe } from 'app/shared/date';
 import { IPopulation } from '../population.model';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'jhi-population-detail',
   templateUrl: './population-detail.component.html',
-  imports: [SharedModule, RouterModule, DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe],
+  imports: [SharedModule, RouterModule, ReactiveFormsModule, FormatMediumDatetimePipe, FormatMediumDatePipe],
 })
 export class PopulationDetailComponent {
   population = input<IPopulation | null>(null);
-
+  formGroup = new FormGroup({
+    lignes: new FormControl(''),
+    colonnes: new FormControl(''),
+  });
   previousState(): void {
     window.history.back();
   }
