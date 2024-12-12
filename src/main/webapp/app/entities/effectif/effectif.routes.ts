241a4d58c3d8981522748cecf0dcc4cb97c15adb
olivier coussieu <olivier.coussieu@orange.fr>
2024-12-12 17:56:14 +0100

    transfert

--- src/main/webapp/app/entities/effectif/effectif.routes.ts
+++ src/main/webapp/app/entities/effectif/effectif.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { UserRouteAccessService } from 'app/core/auth/user-route-access.service';
 import { ASC } from 'app/config/navigation.constants';
 import EffectifResolve from './route/effectif-routing-resolve.service';
+import EffectifAttributResolve from './route/effectif-routing-attribut-resolve.service';
 
 const effectifRoute: Routes = [
   {
@@ -10,6 +11,14 @@ const effectifRoute: Routes = [
     loadComponent: () => import('./list/effectif.component').then(m => m.EffectifComponent),
     data: {
       defaultSort: `id,${ASC}`,
+    },
+    canActivate: [UserRouteAccessService],
+  },
+  {
+    path: ':tab/:lin/:col',
+    loadComponent: () => import('./detail/effectif-detail.component').then(m => m.EffectifDetailComponent),
+    resolve: {
+      effectif: EffectifResolve,
     },
     canActivate: [UserRouteAccessService],
   },
